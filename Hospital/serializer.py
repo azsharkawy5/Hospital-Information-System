a0dccb850ad5d06610c627ce9d24a2917e3e75a3
Mahmoud <mahmouddabdelfattah72@gmail.com>
2023-04-08 10:15:45 +0200

    add nurse serializer

--- Hospital/serializer.py
+++ Hospital/serializer.py
@@ -5,4 +5,10 @@ from .models import *
 class DoctorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Doctor
-        fields = ['id','get_name','specialty','medical_license','department','image']
+        fields = ['id','user','get_name','specialty','medical_license','department','image']
+
+
+class NurseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Nurse
+        fields = ['user','__str__','specialty','medical_license']
