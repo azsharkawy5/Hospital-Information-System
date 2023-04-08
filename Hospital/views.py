8b2c02bed66f27371aea420444f104aaece0c68d
Mahmoud <mahmouddabdelfattah72@gmail.com>
2023-04-08 09:57:24 +0200

    doctor viewset

--- Hospital/views.py
+++ Hospital/views.py
@@ -2,8 +2,13 @@ from django.http import Http404
 from django.shortcuts import render
 from .models import *
 from django.shortcuts import get_object_or_404
+from rest_framework.viewsets import ModelViewSet
+from .serializer import *
 # Create your views here.
 def media(request,id):
     media = get_object_or_404(Doctor,pk = id).image
     return render(request, 'media/media.html',{'media' : media})
         
+class DoctorViewSet(ModelViewSet):
+    queryset = Doctor.objects.select_related('user').all()
+    serializer_class = DoctorSerializer
