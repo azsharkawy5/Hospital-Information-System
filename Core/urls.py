aea723716faa322a1dfa8c3edd5a6cacb2405717
Mahmoud <mahmouddabdelfattah72@gmail.com>
2023-04-08 12:54:07 +0200

    delete core urls

--- Core/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from .models import *
-from rest_framework_nested import routers 
-
-router = routers.DefaultRouter()
-router.register('user',views.UserViewSet)
-
-urlpatterns = router.urls
