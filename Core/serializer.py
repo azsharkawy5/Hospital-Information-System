1c0adcd81b00c70946c619acbde605dd42c852b1
Mahmoud <mahmouddabdelfattah72@gmail.com>
2023-04-08 10:52:47 +0200

    modify user fields

--- Core/serializer.py
+++ Core/serializer.py
@@ -5,4 +5,4 @@ from .models import *
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['id','first_name','last_name','email','gender','phone','national_id','address']
