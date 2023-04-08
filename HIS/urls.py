3a70724b61329082b7d633e3769c01f0037c8a2f
Mahmoud <mahmouddabdelfattah72@gmail.com>
2023-04-08 11:24:17 +0200

    installing jwt

--- HIS/urls.py
+++ HIS/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('auth/', include('djoser.urls.jwt')),
     path('hospital/',include('Hospital.urls')),
     path('auth/',include('djoser.urls')),
+    path('auth/',include('djoser.urls.jwt')),
     path('useradmin/',include('Core.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
