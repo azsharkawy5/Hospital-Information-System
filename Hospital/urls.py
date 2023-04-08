dbc14c2fb1f8a6171ea211e1057c7fc23c85c6e5
Mahmoud <mahmouddabdelfattah72@gmail.com>
2023-04-08 10:22:55 +0200

    add patient url

--- Hospital/urls.py
+++ Hospital/urls.py
@@ -6,5 +6,6 @@ from rest_framework.routers import DefaultRouter
 router = routers.DefaultRouter()
 router.register('doctor',views.DoctorViewSet)
 router.register('nurse',views.NurseViewSet)
+router.register('patient',views.PatientViewSet)
 
 urlpatterns= router.urls
