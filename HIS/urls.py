9e9a072ff92b65d0f4e4cad03c52b180894a3193
Mahmoud <mahmouddabdelfattah72@gmail.com>
2023-04-08 10:07:36 +0200

    add hospital url

--- HIS/urls.py
+++ HIS/urls.py
@@ -18,12 +18,13 @@ from django.urls import path,include
 from Hospital import views
 from django.conf.urls.static import static
 from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('Appointments/',include('Appointments.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
-    
+    path('hospital/',include('Hospital.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
