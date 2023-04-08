e696e365663bfe887a78b9dcae8cef90f15fb855
Mahmoud <mahmouddabdelfattah72@gmail.com>
2023-04-08 10:02:08 +0200

    doctor url

---
+++ Hospital/urls.py
@@ -0,0 +1,7 @@
+from . import views
+from .models import *
+from rest_framework_nested import routers 
+from rest_framework.routers import DefaultRouter
+
+router = routers.DefaultRouter()
+router.register('doctor',views.DoctorViewSet)
