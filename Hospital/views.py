cb10b2280c5fbab5ed8b11c390f4d57492d01f30
Mahmoud <mahmouddabdelfattah72@gmail.com>
2023-04-08 10:17:43 +0200

    add nurse viewset

--- Hospital/views.py
+++ Hospital/views.py
@@ -1,4 +1,3 @@
-from django.http import Http404
 from django.shortcuts import render
 from .models import *
 from django.shortcuts import get_object_or_404
@@ -12,3 +11,7 @@ def media(request,id):
 class DoctorViewSet(ModelViewSet):
     queryset = Doctor.objects.select_related('user').all()
     serializer_class = DoctorSerializer
+
+class NurseViewSet(ModelViewSet):
+    queryset = Nurse.objects.select_related('user').all()
+    serializer_class = NurseSerializer
