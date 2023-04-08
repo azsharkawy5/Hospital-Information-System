4567dd9d187e5b0eea88877ddca9f9279c0609a5
Mahmoud <mahmouddabdelfattah72@gmail.com>
2023-04-08 11:16:35 +0200

    installing djoser

--- HIS/urls.py
+++ HIS/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('hospital/',include('Hospital.urls')),
+    path('auth/',include('djoser.urls')),
     path('useradmin/',include('Core.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
