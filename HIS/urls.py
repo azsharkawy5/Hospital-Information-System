81745b275aa3c70c5b5cdee271c56ccd55aa95a3
Mahmoud <mahmoudte7a1@gmail.com>
2023-04-12 12:29:43 +0200

    record viewset

--- HIS/urls.py
+++ HIS/urls.py
@@ -28,5 +28,6 @@ urlpatterns = [
     path('hospital/',include('Hospital.urls')),
     path('auth/',include('djoser.urls')),
     path('auth/',include('djoser.urls.jwt')),
+    path('records/',include('Records.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
--- Records/urls.py
+++ Records/urls.py
@@ -1,3 +1,12 @@
 from . import views
 from .models import *
 from rest_framework_nested import routers 
+
+router = routers.DefaultRouter()
+router.register('emergencycontact',views.EmergencyContactViewSet)
+router.register('surgery',views.SurgeryViewSet)
+router.register('visits',views.VisitViewSet)
+router.register('vitals',views.VitalViewSet)
+router.register('medicalrecord',views.MedicalRecordViewSet)
+
+urlpatterns = router.urls
