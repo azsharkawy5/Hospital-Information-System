fbe0a723d298b009b33788453803588ddbf0b975
Mahmoud <mahmoudte7a1@gmail.com>
2023-04-16 10:25:56 +0200

    adding pagination

--- HIS/settings.py
+++ HIS/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     'Core',
     'rest_framework',
     'djoser',
@@ -151,6 +152,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE':5,
 }
 
 SIMPLE_JWT = {
