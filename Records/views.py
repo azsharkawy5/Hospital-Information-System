9dec4557e476d8a1dabb3786dbe8dfcec3f5dea0
Mahmoud <mahmoudte7a1@gmail.com>
2023-04-16 09:49:58 +0200

    optimized record views

--- Records/views.py
+++ Records/views.py
@@ -7,75 +7,77 @@ class EmergencyContactViewSet(ModelViewSet):
     serializer_class = EmergencyContactSerializer
 
     def get_queryset(self):
-        try:
-            return EmergencyContact.objects.select_related('patient__user').filter(patient_id = self.kwargs['patient_pk'])
-        except KeyError:
-            return EmergencyContact.objects.select_related('patient__user').all()
+        queryset = EmergencyContact.objects.select_related('patient__user').all()
+        patient_id = self.request.query_params.get('patient_id')
+        if patient_id is not None:
+            queryset = queryset.filter(patient_id=patient_id)
+        return queryset        
+
     
     def get_serializer_context(self):
-        try:
-            return {'patient_id':self.kwargs['patient_pk']}
-        except KeyError:
-             pass 
+        pateint = self.kwargs.get('patient_pk')
+        return {'patient_id':str(pateint)}
+
      
 class VisitViewSet(ModelViewSet):
     serializer_class = VisitsSerializer
 
     def get_queryset(self):
-        try:
-            return Visits.objects.select_related('patient__user').select_related('nurse__user').select_related('doctor__user').filter(patient_id = self.kwargs['patient_pk'])
-        except KeyError:
-            return Visits.objects.select_related('patient__user').select_related('nurse__user').select_related('doctor__user').all()
+        queryset = Visits.objects.select_related('patient__user').select_related('nurse__user').select_related('doctor__user').all()
+        patient_id = self.request.query_params.get('patient_id')
+        if patient_id is not None:
+            queryset = queryset.filter(patient_id=patient_id)
+        return queryset              
+
         
     def get_serializer_context(self):
-        try:
-            return {'patient_id':self.kwargs['patient_pk'],'doctor_id':self.kwargs['doctor_pk'],'nurse_id':self.kwargs['nurse_pk']}
-        except KeyError:
-            pass
+        pateint = self.kwargs.get('patient_pk')
+        return {'patient_id':str(pateint)}
+
     
 class SurgeryViewSet(ModelViewSet):
     serializer_class = SergeryInfoSerializer
     
     def get_queryset(self):
-        try:
-            return SurgeryInfo.objects.select_related('patient__user').select_related('doctor__user').filter(patient_id = self.kwargs['patient_pk'])
-        except KeyError:
-            return SurgeryInfo.objects.select_related('patient__user').select_related('doctor__user').all()
-        
+        queryset = SurgeryInfo.objects.select_related('patient__user').select_related('doctor__user').all()
+        patient_id = self.request.query_params.get('patient_id')
+        if patient_id is not None:
+            queryset = queryset.filter(patient_id=patient_id)
+        return queryset
+
     def get_serializer_context(self):
-        try:
-            return {'patient_id':self.kwargs['patient_pk']}
-        except KeyError:
-            return {'patient_id':'none'}
-   
+        pateint = self.kwargs.get('patient_pk')
+        return {'patient_id':str(pateint)}
+
 
 class VitalViewSet(ModelViewSet):
     serializer_class = VitalSerializer
 
     def get_queryset(self):
-        try:
-            return Vitals.objects.select_related('patient__user').filter(patient_id = self.kwargs['patient_pk'])
-        except KeyError:
-            return Vitals.objects.select_related('patient__user').all()
+        queryset =  Vitals.objects.select_related('patient__user').all()
+        patient_id = self.request.qeury_params.get('patient_id')
+        if patient_id is not None:
+            queryset = queryset.filter(patient_id=patient_id)
+        return queryset
+
     
     def get_serializer_context(self):
-        try:
-            return {'patient_id':self.kwargs['patient_pk']}
-        except KeyError:
-            pass
+        pateint = self.kwargs.get('patient_pk')
+        return {'patient_id':str(pateint)}
+
 
 class MedicalRecordViewSet(ModelViewSet):
     serializer_class = MedicalRecordSerializer
 
     def get_queryset(self):
-        try:
-            return MedicalRecord.objects.select_related('patient__user').select_related('patient').filter(patient_id=self.kwargs['patient_pk'])
-        except KeyError:
-            return MedicalRecord.objects.select_related('patient__user').select_related('patient').all()
+        queryset = MedicalRecord.objects.select_related('patient__user').select_related('patient').all()
+        patient_id = self.request.query_params.get('patient_id')
+        if patient_id is not None:
+            queryset = queryset.filter(patient_id=patient_id)
+        return queryset        
+
         
     def get_serializer_context(self):
-        try:
-            return {'patient_id':self.kwargs['patient_pk']}
-        except KeyError:
-            pass
-# Create your views here.
+        pateint = self.kwargs.get('patient_pk')
+        return {'patient_id':str(pateint)}        
+
