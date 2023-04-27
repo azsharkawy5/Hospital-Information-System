69b05ff37572a0741ab6cd4672855bf7880608fe
Ahmad Zakaria <77364329+zico-son@users.noreply.github.com>
2023-04-28 01:47:15 +0300

    add roles

--- Core/models.py
+++ Core/models.py
@@ -5,6 +5,27 @@ from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
 
 class User(AbstractUser):
+    doctor = 'doctor'
+    nurse = 'nurse'
+    patient = 'patient'
+    receptionist = 'receptionist'
+    radiologist = 'radiologist'
+    lab = 'lab'
+    medical_secretary = 'medical_secretary'
+    pharmacist = 'pharmacist'
+    admin = 'admin'
+    user_types = [
+        (doctor,'Doctor'),
+        (nurse,'Nurse'),
+        (patient,'Patient'),
+        (receptionist,'Receptionist'),
+        (radiologist,'Radiologist'),
+        (lab,'Lab'),
+        (medical_secretary,'Medical Secretary'),
+        (pharmacist,'Pharmacist'),
+        (admin,'Admin'),
+    ]
+
     genders = [
         ('M','Male'),
         ('F','Female'),
@@ -16,6 +37,7 @@ class User(AbstractUser):
     phone_2 = PhoneNumberField(blank=1,null=1)
     gender = models.CharField(max_length=1,choices=genders,null=1,blank=1)
     national_id = models.CharField(max_length=14,unique=1,null=1,blank=1)
+    role = models.CharField(max_length=255,choices=user_types,default=patient)
 
     
   
