70b3932d7ba063da22969c8c82f582c036c400ec
Mahmoud <mahmouddabdelfattah72@gmail.com>
2023-04-29 00:03:16 +0300

    restore db settings

--- HIS/settings.py
+++ HIS/settings.py
@@ -94,21 +94,16 @@ WSGI_APPLICATION = 'HIS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'his',
-#         'HOST': 'localhost',
-#         'USER': 'root',
-#         'PASSWORD': '1234'
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'his',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '1234'
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
