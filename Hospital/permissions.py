ada77773e4840ecf240d161c47ba5b42f64808fa
Mahmoud <mahmouddabdelfattah72@gmail.com>
2023-04-29 00:03:39 +0300

    add patient permission

--- Hospital/permissions.py
+++ Hospital/permissions.py
@@ -3,4 +3,5 @@ from rest_framework.permissions import BasePermission
 class IsPatient(BasePermission):
 
     def has_permission(self, request, view):
-        return bool(request.user and not request.user.is_staff)
+        print(request.user.role)
+        return bool(request.user.role =='patient' and  request.user)
