12f953d7f7d5334cbc8d43b0ce1490706c3407b3
Mahmoud <mahmouddabdelfattah72@gmail.com>
2023-05-01 00:28:49 +0300

    add custom permissions

--- Hospital/permissions.py
+++ Hospital/permissions.py
@@ -5,3 +5,17 @@ class IsPatient(BasePermission):
     def has_permission(self, request, view):
         print(request.user.role)
         return bool(request.user.role =='patient' and  request.user)
+    
+
+class IsDoctor(BasePermission):
+    def has_permission(self, request, view):
+        print(request.user.role)
+        return bool(request.user.role =='doctor' and  request.user)   
+    
+class IsAdminOrReadOnly(BasePermission):
+     
+     def has_permission(self, request, view):
+        return bool(
+            request.user.role == 'admin' or
+            request.method in ('GET', 'HEAD', 'OPTIONS') 
+        )
