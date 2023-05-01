99c60005ba4ac3bd6f4bc2b6f0b757bd58e8e5fc
Mahmoud <mahmouddabdelfattah72@gmail.com>
2023-05-01 13:07:51 +0300

    add custom permission

--- Hospital/permissions.py
+++ Hospital/permissions.py
@@ -37,10 +37,46 @@ class IsReceptionist(BasePermission):
         return bool(request.user.role =='receptionist' and  request.user)
     
 
+class IsReceptionistOrPatientDoctorReadOnly(BasePermission):
+     
+    def has_permission(self, request, view):
+        return bool(
+            request.user.role == 'receptionist' or
+            request.user.role == 'patient' and request.method == 'GET' 
+            or request.user.role == 'doctor' and request.method == 'GET' 
+            #request.method in ('GET', 'HEAD', 'OPTIONS') 
+        )
+    
 class IsReceptionistOrReadOnly(BasePermission):
      
     def has_permission(self, request, view):
         return bool(
             request.user.role == 'receptionist' or
             request.method in ('GET', 'HEAD', 'OPTIONS') 
+        )
+    
+
+class IsMedicalSecretary(BasePermission):
+
+    def has_permission(self, request, view):
+        print(request.user.role)
+        return bool(request.user.role =='medical_secretary' and  request.user)
+    
+
+class IsMedicalSecretaryOrReadOnly(BasePermission):
+     
+    def has_permission(self, request, view):
+        return bool(
+            request.user.role == 'medical_secretary' or
+            request.method in ('GET', 'HEAD', 'OPTIONS') 
+        )
+
+class IsMedicalSecretaryOrPatientDoctorReadOnly(BasePermission):
+     
+    def has_permission(self, request, view):
+        return bool(
+            request.user.role == 'medical_secretary' or
+            request.user.role == 'patient' and request.method == 'GET' 
+            or request.user.role == 'doctor' and request.method == 'GET' 
+            #request.method in ('GET', 'HEAD', 'OPTIONS') 
         )
