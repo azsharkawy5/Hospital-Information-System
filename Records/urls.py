677aaf6b7688c8fca2317d9366bb2e559a4bebbd
Mahmoud <mahmouddabdelfattah72@gmail.com>
2023-05-01 14:52:41 +0300

    remove nested router

--- Records/urls.py
+++ Records/urls.py
@@ -10,14 +10,7 @@ router.register('surgery',views.SurgeryViewSet,basename='surgeryinfo')
 router.register('visits',views.VisitViewSet,basename='visits')
 router.register('vitals',views.VitalViewSet,basename='vitals')
 router.register('medical-record',views.MedicalRecordViewSet,basename='medicalrecord')
-router.register('patient',Hospital.views.PatientViewSet,basename='patient')
 router.register('all-records',views.PatientRecordsViewSet,basename='all-records')
 
-patient_router = routers.NestedDefaultRouter(router,'patient',lookup ='patient')
-patient_router.register('medical-record',views.MedicalRecordViewSet,basename='medicalrecord')
-patient_router.register('vitals',views.VitalViewSet,basename='patient-vitals')
-patient_router.register('visits',views.VisitViewSet,basename='patient-visits')
-patient_router.register('surgery',views.SurgeryViewSet,basename='patient-surgery')
-patient_router.register('emergency-contact',views.EmergencyContactViewSet,basename='patient-emergencycontact')
 
-urlpatterns = router.urls + patient_router.urls 
+urlpatterns = router.urls 
