be5e34ef5125b45629953547bc4f1532300d15b2
Ahmad Zakaria <77364329+zico-son@users.noreply.github.com>
2023-05-03 02:38:16 +0300

    fix Doctor register model

--- Hospital/admin.py
+++ Hospital/admin.py
@@ -2,15 +2,11 @@ from django.contrib import admin
 from .models import *
 from . import models
 # Register your models here.
-@admin.register(models.Doctor)
-class DoctorAdmin(admin.ModelAdmin):
-    list_display = ['__str__','specialty']
-
+admin.site.register(Doctor)
 admin.site.register(Nurse)
 admin.site.register(Patient)
 admin.site.register(Receptionist)
 admin.site.register(Address)
 #admin.site.register(MedicalSecretary)
-admin.site.register(Department)
 admin.site.register(Specialty)
 admin.site.register(Department)
